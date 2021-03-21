2209c4d5a693892ed493fa41ed981cf7ac8d0129
elishakay <kramer1346@gmail.com>
2021-03-21 12:27:47 +0200

    new libraries

---
+++ api/db/config.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+// First you need to create a connection to the database
+// Be sure to replace 'user' and 'password' with the correct values
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  password: 'password',
+});
+
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all remaining queries are executed
+  // Then sends a quit packet to the MySQL server.
+});
